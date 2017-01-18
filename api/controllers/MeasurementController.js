c8b23e79eaaf63b1f40dfc1bb076d86c44b481aa
Peter Hevesi <peter.hevesi@dfki.de>
2017-01-18 17:01:09 +0100

    Add basic sensor and measurement model

---
+++ api/controllers/MeasurementController.js
@@ -0,0 +1,11 @@
+/**
+ * MeasurementController
+ *
+ * @description :: Server-side logic for managing Measurements
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Measurement.js
@@ -0,0 +1,22 @@
+/**
+ * Measurement.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	sensor: {
+  		model: 'Sensor'
+  	},  	
+  	timestamp: {
+  		type: 'date'
+  	},
+  	value: {
+  		type: 'float'
+  	}
+  }
+};
+
